9ca33312c5ae24b372fe55078f6c1b67d1843af0
IuriiSer <yusergeevi4@gmail.com>
2022-10-18 23:00:39 +0300

    stuf for figures

---
+++ src/lib/models/figures/Colors.ts
@@ -0,0 +1,4 @@
+export enum FugureColors {
+  White = 'WHITE',
+  Black = 'BLACK',
+}
---
+++ src/lib/models/figures/Figures.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class Figures {
+  // Pawn = 'PAWN',
+  // Knight = 'KNIGHT',
+  // Bishop = 'BISHOP',
+  // Rook = 'ROOK',
+  // Queen = 'QUEEN',
+  // King = 'KING',
+}
