d1cbe1eec356d6a3e5584c667b25ca79abc6886f
IuriiSer <yusergeevi4@gmail.com>
2022-10-18 22:59:37 +0300

    new ver of board model convert [][] to [] add cells coords by the cell id

--- src/lib/models/board/Board.ts
+++ src/lib/models/board/Board.ts
@@ -1,29 +1,32 @@
-import { Cell } from './Cell';
-import { CellColors } from './CellColors';
-import { CellCoords } from './CellCoords';
+import { CellData } from './Cell'
+import { CellColors } from './CellColors'
+import { CellCoords } from './CellCoords'
 
-export class Board {
-	private _cells: Cell[][] = [];
-	private _cellsCoords: CellCoords = {};
+export class BoardData {
+  private readonly _cells: CellData[] = []
+  private isInited: boolean = false
+  private _cellsCoords: CellCoords = {}
 
-	public get cells(): Cell[][] {
-		return this._cells;
-	}
+  public get cells (): CellData[] {
+    return this._cells
+  }
 
-	public initCells = (): void => {
-		const boadSize = 8;
-		for (let rowInd = 0; rowInd < boadSize; rowInd += 1) {
-			const cellsRow: Cell[] = [];
-			for (let colInd = 0; rowInd < boadSize; rowInd += 1) {
-				if ((rowInd * boadSize + colInd) % 2) {
-					const cell = new Cell({ color: CellColors.Light });
-					cellsRow.push(cell);
-					this._cellsCoords[cell.id] = { row: rowInd, col: colInd };
-					continue;
-				}
-				cellsRow.push(new Cell({ color: CellColors.Dark }));
-			}
-			this._cells.push(cellsRow);
-		}
-	};
+  public initCells = (): void => {
+    if (this.isInited) return
+    const boardSize = 8
+    for (let rowInd = 0; rowInd < boardSize; rowInd += 1) {
+      for (let colInd = 0; colInd < boardSize; colInd += 1) {
+        if ((rowInd + colInd) % 2 === 0) {
+          const cell = new CellData({ color: CellColors.Light })
+          this._cells.push(cell)
+          this._cellsCoords[cell.id] = { row: rowInd, col: colInd }
+          continue
+        }
+        const cell = new CellData({ color: CellColors.Dark })
+        this._cells.push(cell)
+        this._cellsCoords[cell.id] = { row: rowInd, col: colInd }
+      }
+    }
+    this.isInited = true
+  }
 }
