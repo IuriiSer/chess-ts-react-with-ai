eb603a324420a77bea72f2a95227a6a538bf3fbc
IuriiSer <yusergeevi4@gmail.com>
2022-10-18 21:23:12 +0300

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Board } from './lib/models/board/Board';
+import { GameManager } from './lib/models/gameNamager/GameNamager';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [board, setBoard] = useState(new Board());
+
+  console.log(board);
+
+  useEffect(()=> {
+    setTimeout(()=> {
+      setBoard(board)
+    }, 10000)
+  })
+
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
