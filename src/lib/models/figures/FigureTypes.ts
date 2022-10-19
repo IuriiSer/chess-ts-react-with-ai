a959d0c138930893083f2c213e5e05cc30ce1b30
IuriiSer <yusergeevi4@gmail.com>
2022-10-19 23:50:02 +0300

    some fixes / removes

---
+++ src/lib/models/figures/FigureTypes.ts
@@ -0,0 +1,8 @@
+export enum FigureTypes {
+  Pawn = 'PAWN',
+  Knight = 'KNIGHT',
+  Bishop = 'BISHOP',
+  Rook = 'ROOK',
+  Queen = 'QUEEN',
+  King = 'KING',
+}
--- src/lib/models/figures/Figures.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class Figures {
-  // Pawn = 'PAWN',
-  // Knight = 'KNIGHT',
-  // Bishop = 'BISHOP',
-  // Rook = 'ROOK',
-  // Queen = 'QUEEN',
-  // King = 'KING',
-}
