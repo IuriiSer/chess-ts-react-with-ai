d2c033b2ad42b08d101412547147b91403150f49
IuriiSer <yusergeevi4@gmail.com>
2022-10-19 23:47:54 +0300

    rebuild board date -> cell -> to Map

--- src/lib/models/board/Board.ts
+++ src/lib/models/board/Board.ts
@@ -1,32 +1,43 @@
-import { CellData } from './Cell'
+import { CellData, CellID } from './Cell'
 import { CellColors } from './CellColors'
-import { CellCoords } from './CellCoords'
+import { CellCoord } from './CellCoords'
 
 export default class BoardData {
-  private readonly _cells: CellData[] = []
+  protected boardSize: number = 8
   private isInited: boolean = false
-  private _cellsCoords: CellCoords = {}
+  private readonly _cells: CellData[] = []
+  private readonly cellsCoords: Map<CellID, CellCoord> = new Map()
+
+  public initCells = (): void => {
+    if (this.isInited) return
+    for (let rowInd = 0; rowInd < this.boardSize; rowInd += 1) {
+      for (let colInd = 0; colInd < this.boardSize; colInd += 1) {
+        const isLight: boolean = (rowInd + colInd) % 2 === 0
+        const cell = isLight
+          ? new CellData({ color: CellColors.Light })
+          : new CellData({ color: CellColors.Dark })
+        this._cells.push(cell)
+        this.cellsCoords.set(cell.id, { row: rowInd, col: colInd })
+      }
+    }
+    this.isInited = true
+  }
 
   public get cells (): CellData[] {
     return this._cells
   }
 
-  public initCells = (): void => {
-    if (this.isInited) return
-    const boardSize = 8
-    for (let rowInd = 0; rowInd < boardSize; rowInd += 1) {
-      for (let colInd = 0; colInd < boardSize; colInd += 1) {
-        if ((rowInd + colInd) % 2 === 0) {
-          const cell = new CellData({ color: CellColors.Light })
-          this._cells.push(cell)
-          this._cellsCoords[cell.id] = { row: rowInd, col: colInd }
-          continue
-        }
-        const cell = new CellData({ color: CellColors.Dark })
-        this._cells.push(cell)
-        this._cellsCoords[cell.id] = { row: rowInd, col: colInd }
-      }
-    }
-    this.isInited = true
+  public getCellByCoords: (coords: CellCoord) => CellData | null = (coords: CellCoord) => {
+    return this._cells[this.boardSize * coords.row + coords.col] ?? null
+  }
+
+  public getCellByID: (id: CellID) => CellData | null = (id: CellID) => {
+    const coords = this.getCellCoordsByID(id)
+    if (coords == null) return null
+    return this._cells[this.boardSize * coords.row + coords.col]
+  }
+
+  public getCellCoordsByID: (id: CellID) => CellCoord | null = (id: CellID) => {
+    return this.cellsCoords.get(id) ?? null
   }
 }
